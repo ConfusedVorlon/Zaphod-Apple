7b4e159a8cdfd02a8fcdfc700503bfbc68a77868
Rob Jonson <rob@hobbyistsoftware.com>
2021-11-08 15:46:04 +0000

    Example iOS

---
+++ Example_iOS/Example_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example_iOS
+//
+//  Created by Rob Jonson on 08/11/2021.
+//
+
+import UIKit
+import Zaphod
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func debugReset(_ sender: Any) {
+        Zaphod.shared.debugReset()
+    }
+    
+}
+
