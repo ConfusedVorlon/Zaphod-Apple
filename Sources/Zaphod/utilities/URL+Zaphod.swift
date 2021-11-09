e40088d3f877867e8d6fe17f3137323e558660c7
Rob Jonson <rob@hobbyistsoftware.com>
2021-11-09 13:10:15 +0000

    subscriptions url

---
+++ Sources/Zaphod/utilities/URL+Zaphod.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rob Jonson on 09/11/2021.
+//
+
+import Foundation
+
+extension URL {
+    
+    /// URL to open the 'manage subscriptions' interface on iOS or MacOS
+    static let manageSubscriptions = URL(string:"https://buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/manageSubscriptions")!
+}
